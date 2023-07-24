0f72aaeaae0589da27e8c8d1b6c52dfa5827babd
eusoncode <git@github.com:eusoncode/Kata.git>
2023-07-24 02:29:28 -0600

    Add process.argv

---
+++ index.js
@@ -0,0 +1,15 @@
+const { fetchBreedDescription } = require('./breedFetcher');
+
+const breedName = process.argv.slice(2);
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    if (desc === null) {
+      console.log(/*`The Breed: "${breedName}" you specified exists.`*/'Error fetch details:', error);
+    } else {
+      console.log(desc);
+    }
+  }
+});
