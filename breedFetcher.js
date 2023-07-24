d06dab0fdfda944263bd39372814574e288c6da0
eusoncode <git@github.com:eusoncode/Kata.git>
2023-07-24 02:33:14 -0600

    Edit the callbacks in the request

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,21 @@
 const request = require('request');
-const bre = process.argv.slice(2).slice(0, 3);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${bre}`, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else {
+const fetchBreedDescription = function(breedName, callback) {
+  const bre = breedName.slice(0, 3);
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${bre}`, (error, response, body) => {
+    if (error) return callback(error, null);
+
     const data = JSON.parse(body);
     if (data.length === 0) {
-      console.log(`The Breed: "${bre}" specified is not found.`);
+      callback(error, null);
+      return;
     } else {
       let objDescription = data[0].description;
-      console.log(objDescription);
+      callback(null, objDescription);
     }
-  }
-});
+
+  });
+};
+
+module.exports = { fetchBreedDescription };
+
